f0ace23cfb1a158a46b69f852e3130a0ab2843d8
Ken760 <49761163+Ken760@users.noreply.github.com>
2021-02-02 15:24:59 +0300

    Update models.py

--- django_movie/movies/models.py
+++ django_movie/movies/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from datetime import date
 
 class Category(models.Model):
     """Категории"""
@@ -33,3 +33,39 @@ class Actor(models.Model):
 class Genre(models.Model):
     """Жанры"""
     name = models.CharField("Имя", max_length=100)
+    description = models.TextField("Описание")
+    url = models.SlugField(max_length=160, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Жанр"
+        verbose_name_plural = "Жанры"
+
+
+class Movie(models.Model):
+    """Фильм"""
+    title = models.CharField("Название", max_length=100)
+    tagline = models.CharField("Слоган", max_length=100, default='')
+    description = models.TextField("Описание")
+    poster = models.ImageField("Постер", upload_to="movies/")
+    year = models.PositiveSmallIntegerField("Дата выхода", default=2021)
+    country = models.CharField("Страна", max_length=30)
+    directors = models.ManyToManyField(Actor, verbose_name="режиссер", related_name="film_director")
+    actors = models.ManyToManyField(Actor, verbose_name="актеры", related_name="film_actor")
+    genres = models.ManyToManyField(Genre, verbose_name="жанры")
+    world_premiere = models.DateField("Примьера в мире", default=date.today)
+    budget = models.PositiveIntegerField("Бюджет", default=0, help_text="указывать сумму в долларах")
+    fees_in_usa = models.PositiveIntegerField("Сборы в США", default=0, help_text="указать сумму в долларах")
+    fees_in_world = models.PositiveIntegerField("Сборы в Мире", default=0, help_text="указать сумму в долларах")
+    category = models.ForeignKey(Category, verbose_name="Категория", on_delete=models.SET_NULL, null=True)
+    url = models.SlugField(max_length=160, unique=True)
+    draft = models.BooleanField("Черновик", default=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Фильм"
+        verbose_name_plural = "Фильмы"
