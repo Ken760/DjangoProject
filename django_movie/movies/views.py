ad4fb6fdb2be3cf09c284f40ff834ab5b7cd0817
Ken760 <49761163+Ken760@users.noreply.github.com>
2021-02-19 10:46:31 +0300

    changes
    added actors changes models and more

--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic.base import View
+
+from .models import Movie
+
+
+class MoviesView(View):
+    """Список фильмов"""
